9726a5c2a5694c7fbcd1e49923afc7c2d2244430
best <best55599@gmail.com>
2023-11-23 13:09:25 +0700

    finish ex-4

--- ex-4.js
+++ ex-4.js
@@ -2,12 +2,12 @@
 50 + 12; // Expression
 
 //Exercise #4: What is Expression Start coding here
-130 + 300;
+130 + 300; //Expressions
 let zero = 1000 - 1000;
-20 * 120;
-1000 / 2;
-typeof true;
+20 * 120; //Expressions
+1000 / 2; //Expressions
+typeof true; //Expressions
 let name = "John";
-3 > 5;
-10 == 100;
-200;
+3 > 5; //Expressions
+10 == 100; //Expressions
+200; //Expressions
